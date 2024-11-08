c60b5b93e098e6b49fe15ba3565cfd1a5a501c0d
davifariasp <davi.informatica8@gmail.com>
2024-11-07 21:44:31 -0300

    first commit

---
+++ src/app/components/home/home.component.ts
@@ -0,0 +1,44 @@
+import { Component, signal } from '@angular/core';
+import { FooterComponent } from '../footer/footer.component';
+import { HelloWorldService } from '../../services/hello-world.service';
+
+@Component({
+  selector: 'app-home',
+  standalone: true,
+  imports: [FooterComponent],
+  templateUrl: './home.component.html',
+  styleUrl: './home.component.scss'
+})
+export class HomeComponent {
+  name = signal("Davi");
+  cont = signal(0);
+  value = false;
+  dogUrl = "https://www.google.com.br/images/branding/googlelogo/1x/googlelogo_color_272x92dp.png";
+
+  lista = ["A", "B", "C", "D", "E"];
+
+  changeName() {
+    this.cont.update(cont => cont + 1);
+    this.name.set("Testando alteracao" + this.cont());
+  }
+
+  changeValue() {
+    this.value = !this.value;
+  }
+
+  constructor(private helloWorldService: HelloWorldService) {
+    this.helloWorldService.getHelloWorld().subscribe(
+      {
+        next: (data) => {
+          console.log(data);
+        },
+        error: (error) => {
+          console.log(error);
+        },
+        complete: () => {
+          console.log("Complete");
+        }
+      }
+    );
+  }
+}
